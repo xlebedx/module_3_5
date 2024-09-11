4a925ad54478eca6a8ba454f0252ee37c870e50b
xlebedx <xlebedx@gmail.com>
2024-09-12 02:43:35 +0300

    2

--- module_3_5.py
+++ module_3_5.py
@@ -1,10 +1,13 @@
 def get_multiplied_digits(number: int):
     str_number = str(number)
     first = int(str_number[0])
-    if len(str_number) > 1:
-        return first * get_multiplied_digits(int(str_number[1:]))
-    else:
+
+    if len(str_number) <= 1:
         return first
+
+
+    else:
+        return first * get_multiplied_digits(int(str_number[1:]))
 
 
 result = get_multiplied_digits(40203)
